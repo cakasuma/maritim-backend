cedef987056251b6b121dcec2235a8fb5a796464
mustofa-binary <mustofa@regentmarkets.com>
2019-08-30 00:38:49 +0800

    first build

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+let express = require('express')
+let cors = require('cors');
+let app = express()
+let bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    console.log(`${new Date()} => ${req.originalUrl}`, req.body)
+    next()
+})
+
+app.get('/', (req, res) => res.send('Hi welcome to contact API'))
+
+app.use((req, res, next) => {
+    res.status(404).send('We think you are lost')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.info(`Server is listening to ${PORT}`))
